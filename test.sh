bdd50b2d57d8d92fd83ddc12477a0d05cd6af08c
Auric Vente <mprops@protonmail.com>
2024-02-10 10:13:38 -0600

    Mods

--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-venv/bin/python src/borat.py --right=10 --bottom=10 --width=600 --words="Disregard [Random] ; [again] ; Acquire [Random] ; [again] ;"
+venv/bin/python src/borat.py --right=10 --bottom=10 --width=600 \
+--words="Disregard [Random] ; [again] ; Acquire [Random] ; [again] ;"
