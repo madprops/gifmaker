62b5fde16459b50d61c3b7f655bb516170447e6b
Auric Vente <mprops@protonmail.com>
2024-02-12 15:34:39 -0600

    Mods

--- test.sh
+++ test.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-venv/bin/python src/borat.py --right=10 --bottom=10 --width=600 \
---words="Disregard [Random] ; [again] ; Acquire [Random] ; [again] ;"
+venv/bin/python src/borat.py --script="test.toml"
