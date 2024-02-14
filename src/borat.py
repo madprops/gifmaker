d37e8599bb4b57a34cd9d8d99db4d9d98df6a4d5
Auric Vente <mprops@protonmail.com>
2024-02-14 12:15:57 -0600

    Mods

--- src/borat.py
+++ src/borat.py
@@ -8,9 +8,16 @@ import media
 import random
 
 def main():
+	# Fill some paths based on root path
 	args.fill_paths(__file__)
+
+	# Check the provided arguments
 	args.parse_args()
+
+	# Replace [random] with random words
 	words.check_random()
+
+	# Replace [repeat] with repeated lines
 	words.check_repeat()
 
 	# Pick one input path randomly
@@ -23,11 +30,16 @@ def main():
 		# Only resize the frames
 		frames = media.resize_frames(frames)
 	else:
-		# Do all the operations
+		# Apply filters to all the frames
 		frames = media.apply_filters(frames)
+
+		# Add the words to the frames
 		frames = media.word_frames(frames)
+
+		# Resize the frames based on width
 		frames = media.resize_frames(frames)
 
+	# Render and save the output
 	media.render(frames)
 
 if __name__ == "__main__":
